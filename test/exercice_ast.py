c4610a50433545ca6eda2f128bd426c77aa8b9ad
Mosquito74 <nathandu01@gmail.com>
2015-02-12 15:39:21 +0100

    test

---
+++ test/exercice_ast.py
@@ -0,0 +1,7 @@
+a = "*"
+c = 0
+print(a)
+while c<6 :
+    a = a + "*"
+    c = c+1
+    print(a)
---
+++ test/sample_fibonnacci.py
@@ -0,0 +1,8 @@
+print("Suite de Fibonacci : ")
+
+a,b,c = 1,1,1
+
+print(b)
+while c<15 :
+    a,b,c = b,a+b,c+1
+    print(b)
